635a358a592354a5e095f704e100db4c7542eb7e
timwlee <timothyl@adobe.com>
2024-05-18 17:22:52 -0700

    Save work for cards portfolio

---
+++ blocks/cards/cards-portfolio.js
@@ -0,0 +1,122 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function CardsPortfolio (block) {
+  console.log("cards portfolio block: ", block);
+
+  const link = block.querySelector('a');
+  let data = [];
+  block.textContent = '';
+
+  function createCards(groups) {
+    console.log("groups: ", groups);
+  
+    const updatedCards = [];
+  
+    groups.forEach((group) => {
+      if (group.length === 5) {
+        let smallCards = []; // Temporary array to hold small cards
+  
+        group.forEach((item, i) => {
+          if (i === 0 && item.caseStudy === 'true') {
+            const optimizedBgImage = createOptimizedPicture(item.backgroundImage, item.name, true, [{ width: '600' }]);
+            const optimizedLogoImage = createOptimizedPicture(item.logoImage, item.name, true, [{ width: '600' }]);
+  
+            updatedCards.push(`
+              <div class="case-study-card">
+                <div class="wrapper">
+                  <a href="${item.url}">
+                    <div class="pill">Case Study</div>
+                    <div class="card-images">
+                      ${optimizedBgImage.outerHTML}
+                      ${optimizedLogoImage.outerHTML}
+                    </div>
+                  </a>
+                </div>
+              </div>
+            `);
+  
+          } else {
+            console.log("small card: ", item);
+  
+            const optimizedBgImage = createOptimizedPicture(item.backgroundImage, item.name, true, [{ width: '350' }]);
+            const optimizedLogoImage = createOptimizedPicture(item.logoImage, item.name, true, [{ width: '350' }]);
+  
+            smallCards.push(`
+              <div class="small-card">
+                <div class="wrapper">
+                  <div class="card-images">
+                    ${optimizedBgImage.outerHTML}
+                    ${optimizedLogoImage.outerHTML}
+                  </div>
+                  <div class="card-info" style="display:none">
+                    <p>${item.description}</p>
+                    <a href="${item.contactUsLink}"></a>
+                  </div>
+                </div>
+              </div>
+            `);
+          }
+        });
+  
+        // After all small cards for a group have been created, push the entire small-card-container into updatedCards
+        updatedCards.push(`
+          <div class="small-card-container">
+            ${smallCards.join('')}
+          </div>
+        `);
+  
+      } else {
+        //TBD: group of 8
+      }
+    })
+  
+    block.innerHTML = `<div class="portfolio-card-container">${updatedCards.join('')}</div>`;
+  }
+
+  function sortData(data) { //TBD: Need to optimize to support 8 cards
+      let result = [];
+      let temp = [];
+      let count = 0;
+      let caseStudyFound = false;
+
+      for (let i = 0; i < data.length; i++) {
+          temp.push(data[i]);
+          count++;
+
+          if (data[i].caseStudy === "true") {
+              caseStudyFound = true;
+              temp.sort((a, b) => b.caseStudy.localeCompare(a.caseStudy));
+          }
+
+          if ((caseStudyFound && count === 5) || (!caseStudyFound && count === 8)) {
+              result.push(temp);
+              temp = [];
+              count = 0;
+              caseStudyFound = false;
+          }
+      }
+
+      // If there are any remaining items in temp, add them to the result
+      if (temp.length > 0) {
+          result.push(temp);
+      }
+
+      return result;
+  }
+
+  async function initialize() {
+    const response = await fetch(link?.href);
+
+    if (response.ok) {
+      const jsonData = await response.json();
+      data = jsonData?.data;
+      
+      let sortedGroups = sortData(data);
+      createCards(sortedGroups);
+    } else {
+      console.log("Unable to get json data for cards portfolio");
+    }
+  }
+
+  initialize();
+}
