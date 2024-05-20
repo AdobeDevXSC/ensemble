2b371ee7220570cef4129635fc53475426d39924
timwlee <timothyl@adobe.com>
2024-05-20 15:30:55 -0700

    Update default cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,8 +3,6 @@ import CardsPortfolio from './cards-portfolio.js';
 import CardsCustomers from './cards-customers.js';
 
 export default function decorate(block) {
-  // console.log("Cards block: ", block);
-
   const isPortfolio = block.classList.contains('portfolio');
   const isCustomers = block.classList.contains('customers');
 
@@ -16,19 +14,40 @@ export default function decorate(block) {
     CardsCustomers(block);
   }
 
-  
-  // /* change to ul, li */
-  // const ul = document.createElement('ul');
-  // [...block.children].forEach((row) => {
-  //   const li = document.createElement('li');
-  //   while (row.firstElementChild) li.append(row.firstElementChild);
-  //   [...li.children].forEach((div) => {
-  //     if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-  //     else div.className = 'cards-card-body';
-  //   });
-  //   ul.append(li);
-  // });
-  // ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  // block.textContent = '';
-  // block.append(ul);
+  if (!isPortfolio && !isCustomers) {
+    block.classList.add('default');
+
+    /* change to ul, li */
+    const ul = document.createElement('ul');
+    [...block.children].forEach((row) => {
+        const li = document.createElement('li');
+        while (row.firstElementChild) li.append(row.firstElementChild);
+        [...li.children].forEach((div) => {
+            if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+            else div.className = 'cards-card-body';
+        });
+        ul.append(li);
+    });
+    ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+
+    /* Modify a tags */
+    ul.querySelectorAll('li').forEach((li) => {
+        const a = li.querySelector('a');
+        const href = a.href;
+
+        a.parentElement.remove();
+
+        const newA = document.createElement('a');
+        newA.href = href;
+
+        while (li.firstChild) {
+            newA.appendChild(li.firstChild);
+        }
+        
+        li.appendChild(newA);
+    });
+
+    block.textContent = '';
+    block.append(ul);
+  }
 }
