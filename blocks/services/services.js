e2c02ae2a62a7e5856a72f48af8fb053578762a8
timwlee <timothyl@adobe.com>
2024-07-09 11:35:06 -0700

    Set up for workshop

--- blocks/services/services.js
+++ blocks/services/services.js
@@ -1,42 +1,44 @@
 export default function decorate(block) { 
-  // Create a container for the service cards
-  const cardContainer = document.createElement('div');
-  cardContainer.classList.add('card-container');
+  console.log("services block: ", block);
 
-  // Iterate over each child element within the block
-  [...block.children].forEach((child) => {
-    // Add a class to each child element for clearer CSS targeting
-    child.classList.add('card');
+  // // Create a container for the service cards
+  // const cardContainer = document.createElement('div');
+  // cardContainer.classList.add('card-container');
 
-    // Assign a unique class to the div containing the image
-    const imageWrapper = child.firstElementChild;
-    imageWrapper.classList.add('image-wrapper');
+  // // Iterate over each child element within the block
+  // [...block.children].forEach((child) => {
+  //   // Add a class to each child element for clearer CSS targeting
+  //   child.classList.add('card');
 
-    // Assign a unique class to the div containing the card information
-    const infoWrapper = child.lastElementChild;
-    infoWrapper.classList.add('info-wrapper');
+  //   // Assign a unique class to the div containing the image
+  //   const imageWrapper = child.firstElementChild;
+  //   imageWrapper.classList.add('image-wrapper');
 
-    // Find and remove the last <p> element that contains the link. Keep track of href value. 
-    const lastParagraph = infoWrapper.querySelector('p:last-of-type');
-    const link = lastParagraph.querySelector('a');
-    let href = '#';
+  //   // Assign a unique class to the div containing the card information
+  //   const infoWrapper = child.lastElementChild;
+  //   infoWrapper.classList.add('info-wrapper');
 
-    if (lastParagraph.contains(link)) {
-      href = link?.href;
-      lastParagraph.remove();
-    }
+  //   // Find and remove the last <p> element that contains the link. Keep track of href value. 
+  //   const lastParagraph = infoWrapper.querySelector('p:last-of-type');
+  //   const link = lastParagraph.querySelector('a');
+  //   let href = '#';
+
+  //   if (lastParagraph.contains(link)) {
+  //     href = link?.href;
+  //     lastParagraph.remove();
+  //   }
   
-    // Create a new <a> element to wrap the entire card, enabling redirect on click
-    const wrapperLink = document.createElement('a');
-    wrapperLink.href = href;
+  //   // Create a new <a> element to wrap the entire card, enabling redirect on click
+  //   const wrapperLink = document.createElement('a');
+  //   wrapperLink.href = href;
 
-    // Append the child element into the wrapper link
-    wrapperLink.appendChild(child);
+  //   // Append the child element into the wrapper link
+  //   wrapperLink.appendChild(child);
 
-    // Append the wrapper link to the card container
-    cardContainer.appendChild(wrapperLink);
-  });
+  //   // Append the wrapper link to the card container
+  //   cardContainer.appendChild(wrapperLink);
+  // });
 
-  // Append the card container to the block element
-  block.appendChild(cardContainer);
+  // // Append the card container to the block element
+  // block.appendChild(cardContainer);
 }
